02a23a4f666a1c43844cf3f58921ebcdf5e6b92b
Sparash sharma <sparash.27kpr@gmail.com>
2018-10-06 11:56:00 +0000

    assignment10

--- filehandle.java
+++ filehandle.java
@@ -1,13 +1,33 @@
 import java.io.*;
-class filehandle
+class dynamic
 {
-    public static void main(String[] args) throws IOException
-    {
-        File f2=new File("MyFolder"); //Representing not creating a file in MyFolder
-        f2.mkdir();
-        File f1=new File(f2,"MyFile.txt");
-        f1.createNewFile();
-        File f3=new File("myFolder2/test/FileHandling");
-        f3.mkdirs();
-    }
+	public static void main(String[] args)
+	{
+		File f=new File("C:/Users/DELL/Documents/Dynamic Data Storage assignment/abc.txt");
+		try
+		{
+			FileInputStream f1=new FileInputStream(f);
+			int i=f1.read();
+			int lineCount=1;
+			System.out.print("Path "+lineCount + ": ");
+			while(i!=-1)
+			{
+				if((char)i=='\n')
+				{
+					System.out.println();
+					lineCount++;
+					System.out.print("Path "+ lineCount + ": ");
+				}
+				else
+					System.out.print((char)i);
+				i=f1.read();
+			}
+			f1.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+		System.out.println("\n");
+	}
 }
